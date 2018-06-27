eff3963b3c42868f5f81fa3a2214b42e90f26b28
Ryan King <ryan@theryanking.com>
2018-06-26 17:00:08 -0700

    refactor apply to write tests (#6)
    * test applyTemplate
    
    * refactor and test touch functionality
    
    * add assert for nil error
    
    * refactor createFile and test

--- apply/apply_test.go
+++ apply/apply_test.go
@@ -1,8 +1,12 @@
 package apply
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,4 +19,112 @@ func TestRemoveExtension(t *testing.T) {
 
 	x = removeExtension("foo.txt.asdf")
 	assert.Equal(t, "foo.txt", x)
+}
+
+func TestApplyTemplateBasic(t *testing.T) {
+	sourceFile := strings.NewReader("foo")
+	dest := afero.NewMemMapFs()
+	path := "bar"
+	overrides := struct{ Foo string }{"foo"}
+
+	e := applyTemplate(sourceFile, dest, path, overrides)
+	assert.Nil(t, e)
+	f, e := dest.Open("bar")
+	assert.Nil(t, e)
+	r, e := ioutil.ReadAll(f)
+	assert.Nil(t, e)
+	assert.Equal(t, "foo", string(r))
+}
+
+func TestApplyTemplate(t *testing.T) {
+	sourceFile := strings.NewReader("Hello {{.Name}}")
+	dest := afero.NewMemMapFs()
+	path := "hello"
+	overrides := struct{ Name string }{"World"}
+
+	e := applyTemplate(sourceFile, dest, path, overrides)
+	assert.Nil(t, e)
+	f, e := dest.Open("hello")
+	assert.Nil(t, e)
+	r, e := ioutil.ReadAll(f)
+	assert.Nil(t, e)
+	assert.Equal(t, "Hello World", string(r))
+}
+
+func TestTouchFile(t *testing.T) {
+	fs := afero.NewMemMapFs()
+
+	e := touchFile(fs, "foo")
+	assert.Nil(t, e)
+	r, e := readFile(fs, "foo")
+	assert.Nil(t, e)
+	assert.Equal(t, "", r)
+
+	fs = afero.NewMemMapFs()
+
+	writeFile(fs, "asdf", "jkl")
+
+	r, e = readFile(fs, "asdf")
+	assert.Nil(t, e)
+	assert.Equal(t, "jkl", r)
+
+	e = touchFile(fs, "asdf")
+	assert.Nil(t, e)
+	r, e = readFile(fs, "asdf")
+	assert.Nil(t, e)
+	assert.Equal(t, "jkl", r)
+
+}
+
+func TestCreateFile(t *testing.T) {
+	fs := afero.NewMemMapFs()
+
+	// create new file
+
+	e := createFile(fs, "foo", strings.NewReader("bar"))
+	assert.Nil(t, e)
+
+	r, e := readFile(fs, "foo")
+	assert.Nil(t, e)
+	assert.Equal(t, "bar", r)
+
+	// not overwrite existing file
+
+	fs = afero.NewMemMapFs()
+
+	e = createFile(fs, "foo", strings.NewReader("bar"))
+	assert.Nil(t, e)
+
+	r, e = readFile(fs, "foo")
+	assert.Nil(t, e)
+	assert.Equal(t, "bar", r)
+
+	e = createFile(fs, "foo", strings.NewReader("BAM"))
+	assert.Nil(t, e)
+
+	r, e = readFile(fs, "foo")
+	assert.Nil(t, e)
+	assert.Equal(t, "bar", r)
+
+}
+
+func readFile(fs afero.Fs, path string) (string, error) {
+	f, e := fs.Open(path)
+	if e != nil {
+		return "", e
+	}
+	r, e := ioutil.ReadAll(f)
+	if e != nil {
+		return "", e
+	}
+	return string(r), nil
+}
+
+func writeFile(fs afero.Fs, path string, contents string) error {
+	f, e := fs.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if e != nil {
+		return e
+	}
+	_, e = f.WriteString(contents)
+	return e
 }
