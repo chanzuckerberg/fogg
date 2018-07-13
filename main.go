1991726e191e72a98b0f161d90778d5edf59c597
Ryan King <rking@chanzuckerberg.com>
2018-07-13 13:42:11 -0700

    set logrus formatter

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/chanzuckerberg/fogg/cmd"
+import (
+	"github.com/chanzuckerberg/fogg/cmd"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
+	formatter := &log.TextFormatter{
+		DisableTimestamp: true,
+	}
+	log.SetFormatter(formatter)
 	cmd.Execute()
 }
