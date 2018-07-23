82af9c98a96ebbac3667285b6917d3ebfce38b18
Ryan King <rking@chanzuckerberg.com>
2018-07-23 11:26:35 -0700

    add config for module_source

--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"bufio"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
@@ -9,7 +11,7 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-func TestParse(t *testing.T) {
+func TestParseDefaults(t *testing.T) {
 	json := `
 	{
 		"defaults": {
@@ -35,6 +37,37 @@ func TestParse(t *testing.T) {
 	assert.Equal(t, "the-bucket", c.Defaults.InfraBucket)
 	assert.Equal(t, "test-project", c.Defaults.Project)
 	assert.Equal(t, "0.11.0", c.Defaults.TerraformVersion)
+}
+
+func TestParse(t *testing.T) {
+	f, _ := os.Open("fixtures/full.json")
+	defer f.Close()
+	r := bufio.NewReader(f)
+	c, e := ReadConfig(r)
+	assert.Nil(t, e)
+	assert.NotNil(t, c.Defaults)
+	assert.Equal(t, int64(1), *c.Defaults.AccountID)
+	assert.Equal(t, "us-west-2", c.Defaults.AWSRegionBackend)
+	assert.Equal(t, "us-west-1", c.Defaults.AWSRegionProvider)
+	assert.Equal(t, "0.1.0", c.Defaults.AWSProviderVersion)
+	assert.Equal(t, "czi", c.Defaults.AWSProfileBackend)
+	assert.Equal(t, "czi", c.Defaults.AWSProfileProvider)
+	assert.Equal(t, "the-bucket", c.Defaults.InfraBucket)
+	assert.Equal(t, "test-project", c.Defaults.Project)
+	assert.Equal(t, "0.11.0", c.Defaults.TerraformVersion)
+
+	assert.NotNil(t, c.Accounts)
+	assert.Len(t, c.Accounts, 2)
+
+	assert.NotNil(t, c.Envs)
+	assert.Len(t, c.Envs, 1)
+	assert.Len(t, c.Envs["stage"].Components, 1)
+	env := c.Envs["stage"].Components["env"]
+	assert.NotNil(t, env)
+	assert.NotNil(t, env.ModuleSource)
+	assert.Equal(t, "github.com/foo/bar", *env.ModuleSource)
+
+	assert.NotNil(t, c.Modules)
 }
 
 func TestJsonFailure(t *testing.T) {
