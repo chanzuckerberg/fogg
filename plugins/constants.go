50d0fa9987e0b04088d9bad1851d76b34c08a138
edulop <elopez@chanzuckerberg.com>
2018-08-15 12:41:04 -0700

    fmt

--- plugins/constants.go
+++ plugins/constants.go
@@ -4,7 +4,6 @@ const (
 	// TerraformPluginCacheDir is the directory where terraform caches tf approved providers
 	// See https://www.terraform.io/docs/configuration/providers.html#provider-plugin-cache
 	TerraformPluginCacheDir = ".terraform.d/plugin-cache"
-
 	// TerraformCustomPluginCacheDir is the directory used by terraform to search for custom providers
 	// We default to linux_amd64 since we're running terraform inside of docker
 	// We vendor providers here
