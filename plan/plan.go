e44529b13b09d115f80f1affeb5c09e2b4ea94c8
Eduardo Lopez <elopez@chanzuckerberg.com>
2018-08-16 10:23:20 -0700

    bump image version to 0.2.1 #120

--- plan/plan.go
+++ plan/plan.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// The version of the chanzuckerberg/terraform docker image to use
-	dockerImageVersion = "0.2.0"
+	dockerImageVersion = "0.2.1"
 )
 
 type AWSConfiguration struct {
