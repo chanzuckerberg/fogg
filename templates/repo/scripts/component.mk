2c5fca64129e4a1f28fc608dc21a123bd3d42532
Eduardo Lopez <elopez@chanzuckerberg.com>
2018-10-31 17:20:18 -0700

    Fix docker interactive flags not getting evaluated properly #183

--- templates/repo/scripts/component.mk
+++ templates/repo/scripts/component.mk
@@ -13,15 +13,15 @@ IMAGE_VERSION=$(DOCKER_IMAGE_VERSION)_TF$(TERRAFORM_VERSION)
 # dependencies.mk helps with dependencies such as terraform
 include: ./dependencies.mk
 
-docker_base := \
+docker_base = \
 	docker run --rm -e HOME=/home -v $$HOME/.aws:/home/.aws -v $(REPO_ROOT):/repo \
 	-v $(REPO_ROOT)/.bin:/usr/local/bin -v $(REPO_ROOT)/terraform.d:/repo/$(REPO_RELATIVE_PATH)/terraform.d \
 	-e GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' \
 	-e RUN_USER_ID=$(shell id -u) -e RUN_GROUP_ID=$(shell id -g) \
 	-e TF_PLUGIN_CACHE_DIR="/repo/.terraform.d/plugin-cache" -e TF="$(TF)" \
 	-w /repo/$(REPO_RELATIVE_PATH) $(TF_VARS) $(FOGG_DOCKER_FLAGS) $$(sh $(REPO_ROOT)/scripts/docker-ssh-mount.sh)
-docker_terraform := $(docker_base) chanzuckerberg/terraform:$(IMAGE_VERSION)
-docker_sh := $(docker_base) --entrypoint='/bin/sh' chanzuckerberg/terraform:$(IMAGE_VERSION)
+docker_terraform = $(docker_base) chanzuckerberg/terraform:$(IMAGE_VERSION)
+docker_sh = $(docker_base) --entrypoint='/bin/sh' chanzuckerberg/terraform:$(IMAGE_VERSION)
 
 ifdef FOGG_DISABLE_DOCKER
 	sh_command ?= $(SHELL)
