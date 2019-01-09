73705d09e9a3a8dab33dd412451a03f17c353bb1
Ryan King <rking@chanzuckerberg.com>
2019-01-09 13:13:03 -0800

    refactor to make cmd bootstrapping more DRY

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,14 +1,29 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/chanzuckerberg/fogg/setup"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	applyCmd.Flags().StringP("config", "c", "fogg.json", "Use this to override the fogg config file.")
+	applyCmd.Flags().BoolP("verbose", "v", false, "use this to turn on verbose output")
+}
 
 var setupCmd = &cobra.Command{
 	Use:           "setup",
 	Short:         "Setup dependencies for curent working directory",
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		setupDebug(debug)
+		fs, config, err := bootstrapCmd(cmd, debug)
 
-		return nil
+		if err != nil {
+			return err
+		}
+
+		// check that we are at root of initialized git repo
+		openGitOrExit(fs)
+
+		return setup.Setup(fs, config)
 	},
 }
