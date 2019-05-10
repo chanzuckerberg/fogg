e7a7ea8275950ef58e8c25bdce9e55d2f9386307
Ryan King <rking@chanzuckerberg.com>
2019-05-10 13:51:47 -0700

    switch to use Symlinker

--- apply/apply_test.go
+++ apply/apply_test.go
@@ -191,14 +191,15 @@ func TestCreateFileNonExistentDirectory(t *testing.T) {
 }
 
 func TestApplySmokeTest(t *testing.T) {
-	t.Skip("doesn't currently work because afero doesn't support symlinks")
-	// We have to use a BasePathFs so that we can calculate `RealPath` for symlinking. Afero doesn't support symlinks
 	fs := afero.NewBasePathFs(afero.NewMemMapFs(), "/")
 	json := `
 {
   "defaults": {
-    "aws_region": "reg",
-    "aws_profile": "prof",
+    "aws_region_provider": "reg",
+    "aws_region_backend": "reg",
+    "aws_profile_provider": "prof",
+    "aws_profile_backend": "prof",
+    "aws_provider_version": "0.12.0",
     "infra_s3_bucket": "buck",
     "project": "proj",
     "terraform_version": "0.100.0",
@@ -235,6 +236,9 @@ func TestApplySmokeTest(t *testing.T) {
 	c, e := v1.ReadConfig([]byte(json))
 	assert.NoError(t, e)
 	c2, e := config.UpgradeConfigVersion(c)
+	assert.NoError(t, e)
+
+	e = c2.Validate()
 	assert.NoError(t, e)
 
 	e = Apply(fs, c2, templates.Templates, false)
