26080859f484353ab539aeb85b82e5ed03a11b67
edulop <elopez@chanzuckerberg.com>
2019-05-29 16:12:24 -0700

    vendor

--- vendor/github.com/segmentio/go-prompt/prompt.go
+++ vendor/github.com/segmentio/go-prompt/prompt.go
@@ -1,38 +1,35 @@
 package prompt
 
+import "github.com/howeyc/gopass"
 import "strings"
 import "strconv"
 import "fmt"
-import "github.com/howeyc/gopass"
 
 // String prompt.
-func String(prompt string) string {
+func String(prompt string, args ...interface{}) string {
 	var s string
-	fmt.Printf(prompt + ": ")
+	fmt.Printf(prompt+": ", args...)
 	fmt.Scanln(&s)
 	return s
 }
 
 // String prompt (required).
-func StringRequired(prompt string) string {
-	s := String(prompt)
-	if strings.Trim(s, " ") == "" {
-		return StringRequired(prompt)
-	} else {
-		return s
+func StringRequired(prompt string, args ...interface{}) (s string) {
+	for strings.Trim(s, " ") == "" {
+		s = String(prompt, args...)
 	}
+	return s
 }
 
 // Confirm continues prompting until the input is boolean-ish.
-func Confirm(prompt string) bool {
-	s := String(prompt)
-	switch s {
-	case "yes", "y", "Y":
-		return true
-	case "no", "n", "N":
-		return false
-	default:
-		return Confirm(prompt)
+func Confirm(prompt string, args ...interface{}) bool {
+	for {
+		switch String(prompt, args...) {
+		case "Yes", "yes", "y", "Y":
+			return true
+		case "No", "no", "n", "N":
+			return false
+		}
 	}
 }
 
@@ -71,16 +68,18 @@ func Choose(prompt string, list []string) int {
 }
 
 // Password prompt.
-func Password(prompt string) string {
-	fmt.Printf(prompt + ": ")
-	s := string(gopass.GetPasswd()[0:])
+func Password(prompt string, args ...interface{}) string {
+	fmt.Printf(prompt+": ", args...)
+	password, _ := gopass.GetPasswd()
+	s := string(password[0:])
 	return s
 }
 
 // Password prompt with mask.
-func PasswordMasked(prompt string) string {
-	fmt.Printf(prompt + ": ")
-	s := string(gopass.GetPasswdMasked()[0:])
+func PasswordMasked(prompt string, args ...interface{}) string {
+	fmt.Printf(prompt+": ", args...)
+	password, _ := gopass.GetPasswdMasked()
+	s := string(password[0:])
 	return s
 }
 
