f6d2253c46fe3306ee61c446c617cdfd2128db05
Elijah Chanakira <echanakira@chanzuckerberg.com>
2019-06-17 16:52:22 -0700

    Added yaml tests for v2/config and created new yaml folders in testdata

--- config/v2/config_test.go
+++ config/v2/config_test.go
@@ -31,10 +31,55 @@ func TestReadConfig(t *testing.T) {
 	a.Len(w, 0)
 }
 
+func TestReadConfigYaml(t *testing.T) {
+	a := assert.New(t)
+
+	b, e := util.TestFile("empty")
+	a.NoError(e)
+	c, e := ReadConfig(b)
+	a.NoError(e)
+
+	w, e := c.Validate()
+	a.Error(e)
+	a.Len(w, 0)
+
+	b2, e := util.TestFile("v2_minimal_valid_yaml")
+	a.NoError(e)
+
+	c, e = ReadConfig(b2)
+	a.NoError(e)
+
+	w, e = c.Validate()
+	a.NoError(e)
+	a.Len(w, 0)
+}
+
 func TestReadSnowflakeProvider(t *testing.T) {
 	r := require.New(t)
 
 	b, e := util.TestFile("snowflake_provider")
+	r.NoError(e)
+	r.NotNil(b)
+
+	c, e := ReadConfig(b)
+	r.NoError(e)
+	r.NotNil(c)
+
+	w, e := c.Validate()
+	r.NoError(e)
+	r.Len(w, 0)
+
+	r.NotNil(c.Defaults.Providers)
+	r.NotNil(c.Defaults.Providers.Snowflake)
+	r.Equal("foo", *c.Defaults.Providers.Snowflake.Account)
+	r.Equal("bar", *c.Defaults.Providers.Snowflake.Role)
+	r.Equal("us-west-2", *c.Defaults.Providers.Snowflake.Region)
+}
+
+func TestReadSnowflakeProviderYaml(t *testing.T) {
+	r := require.New(t)
+
+	b, e := util.TestFile("snowflake_provider_yaml")
 	r.NoError(e)
 	r.NotNil(b)
 
@@ -75,3 +120,27 @@ func TestReadBlessProvider(t *testing.T) {
 	r.Equal("0.0.0", *c.Defaults.Providers.Bless.Version)
 	r.Equal([]string{"a", "b"}, c.Defaults.Providers.Bless.AdditionalRegions)
 }
+
+func TestReadBlessProviderYaml(t *testing.T) {
+	r := require.New(t)
+
+	b, e := util.TestFile("bless_provider_yaml")
+	r.NoError(e)
+	r.NotNil(b)
+
+	c, e := ReadConfig(b)
+	r.NoError(e)
+	r.NotNil(c)
+
+	w, e := c.Validate()
+	r.NoError(e)
+	r.Len(w, 0)
+
+	r.NotNil(c.Defaults.Providers)
+	r.NotNil(c.Defaults.Providers.Bless)
+	r.Equal("foofoofoo", *c.Defaults.Providers.Bless.AWSProfile)
+	r.Equal("bar", *c.Defaults.Providers.Bless.AWSRegion)
+	r.Equal("0.0.0", *c.Defaults.Providers.Bless.Version)
+	r.Equal([]string{"a", "b"}, c.Defaults.Providers.Bless.AdditionalRegions)
+}
+
