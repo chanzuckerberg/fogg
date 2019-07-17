59e032b04a8900853951ba486ff0e0a03d5256a8
Elijah Chanakira <elijahchan21@gmail.com>
2019-07-16 18:23:41 -0700

    Renamed variable and negated Prompt

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -14,7 +14,7 @@ type Migration interface {
 }
 
 //RunMigrations cycles through a list of migrations and applies them if necessary
-func RunMigrations(fs afero.Fs, configFile string, ignorePrompts bool) error {
+func RunMigrations(fs afero.Fs, configFile string, forceApply bool) error {
 	migrations := []Migration{
 		&VersionUpgradeMigration{}, //This runs first because v1 to v2 uses json.Marshal
 		&JSONToYamlMigration{},
@@ -30,7 +30,7 @@ func RunMigrations(fs afero.Fs, configFile string, ignorePrompts bool) error {
 		}
 
 		//If the user does not want to run this migration
-		if !ignorePrompts && migration.Prompt() {
+		if !forceApply && !migration.Prompt() {
 			continue
 		}
 
