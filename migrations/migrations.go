188cbc72064db73d0430d82bc7001329538d3523
Elijah Chanakira <elijahchan21@gmail.com>
2019-07-16 18:12:55 -0700

    Fixed conditional

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -14,7 +14,7 @@ type Migration interface {
 }
 
 //RunMigrations cycles through a list of migrations and applies them if necessary
-func RunMigrations(fs afero.Fs, configFile string, skipPrompts bool) error {
+func RunMigrations(fs afero.Fs, configFile string, ignorePrompts bool) error {
 	migrations := []Migration{
 		&VersionUpgradeMigration{}, //This runs first because v1 to v2 uses json.Marshal
 		&JSONToYamlMigration{},
@@ -30,7 +30,7 @@ func RunMigrations(fs afero.Fs, configFile string, skipPrompts bool) error {
 		}
 
 		//If the user does not want to run this migration
-		if !skipPrompts && migration.Prompt() {
+		if !ignorePrompts && migration.Prompt() {
 			continue
 		}
 
