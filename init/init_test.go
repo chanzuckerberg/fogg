2e24f300ba442868f09e88761f7685cfdc610c3e
Elijah Chanakira <elijahchan21@gmail.com>
2019-08-02 10:33:41 -0700

    Added test for init version 0 and latest

--- init/init_test.go
+++ init/init_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/chanzuckerberg/fogg/config"
 	"github.com/chanzuckerberg/fogg/util"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,9 +22,14 @@ func TestInit(t *testing.T) {
 
 	conf := config.InitConfig(project, region, bucket, table, profile, owner, AWSProviderVersion, foggVersion)
 	a.NotNil(conf)
+	a.Equal(foggVersion, conf.Version)
 
 	err = writeConfig(fs, conf)
 	a.NoError(err)
+
+	exists, err := afero.Exists(fs, "fogg.yml")
+	a.NoError(err)
+	a.True(exists)
 }
 
 func TestInitVersion(t *testing.T) {
@@ -37,5 +43,19 @@ func TestInitVersion(t *testing.T) {
 
 	conf := config.InitConfig(project, region, bucket, table, profile, owner, AWSProviderVersion, foggVersion)
 	a.NotNil(conf)
-	// a.Nil(conf.Version)
+	a.Equal(foggVersion, conf.Version)
+}
+
+func TestInitVersionZero(t *testing.T) {
+	a := assert.New(t)
+	project := "acme"
+	region := "us-west-2"
+	bucket := "acme-infra"
+	table := "acme"
+	profile := "acme-auth"
+	owner := "infra@acme.example"
+
+	conf := config.InitConfig(project, region, bucket, table, profile, owner, AWSProviderVersion, 0)
+	a.NotNil(conf)
+	a.Equal(0, conf.Version)
 }
