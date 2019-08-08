363c9eca7de838ffeacc37ab7f1fbae0fc17286f
Elijah Chanakira <elijahchan21@gmail.com>
2019-08-07 17:34:30 -0700

    broken but duplicates removed

--- exp/examine/examine.go
+++ exp/examine/examine.go
@@ -7,16 +7,17 @@ import (
 func Examine(fs afero.Fs, path string) error {
 	//Collect local modules to be updated
 	localModules, err := GetLocalModules(fs, path)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	globalModules, err := LatestModuleVersions(fs, localModules)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	//TODO:(EC)Middleware to compare local and global modules
-	if globalModules != nil {} //To silence "declared and not used" error
+	if globalModules != nil {
+	} //To silence "declared and not used" error
 
 	return nil
 }
