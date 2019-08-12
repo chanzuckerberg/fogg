433fd6840ed6a9c648865e84fb9331d4f42fdb1e
Elijah Chanakira <elijahchan21@gmail.com>
2019-08-12 16:20:49 -0700

    Added new registry resource for testing purposes

--- exp/examine/examine.go
+++ exp/examine/examine.go
@@ -26,6 +26,23 @@ func Examine(fs afero.Fs, path string) error {
 	return nil
 }
 
+//DO I COMPARE THEM RIGHT AWAY or DO I CHECK IF THERE ARE ANY DIFFERENCES AND STORE THOSE
 func isDifferent(config *config.Config, modules []ModuleWrapper) bool {
+	//TODO: Empty check?
+	for _, mod := range config.Modules {
+		for _, modWrap := range modules {
+			if mod.Source == modWrap.moduleSource {
+				compare(mod, modWrap)
+			}
+		}
+	}
+
 	return true
+}
+
+func compare(mod *config.Module, modWrap ModuleWrapper) {
+	// fmt.Println(mod.Source)
+	// fmt.Println(modWrap.moduleSource)
+	// fmt.Printf("Module = %v\n", mod.RawConfig.Variables["var.env"])
+	// fmt.Printf("ModuleWrapper = %v", modWrap.module.Variables["env"])
 }
