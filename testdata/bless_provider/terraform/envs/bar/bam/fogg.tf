c4c3004397f904195f8d3a275e1e0548ae464d0d
Ryan King <rking@chanzuckerberg.com>
2020-02-04 11:06:29 -0800

    [feature] pin null provider version (#399)
    [feature] pin null provider versionWe now pin the version of the terraform null provider. The version is hard-coded, but we can make it configurable when we need to.
    
    This is part of efforts to have reproducible runs & reduce noise in terraform's output.
    
    ### Test Plan
    * travis-ci
    
    ### References
    *

--- testdata/bless_provider/terraform/envs/bar/bam/fogg.tf
+++ testdata/bless_provider/terraform/envs/bar/bam/fogg.tf
@@ -143,3 +143,7 @@ provider template {
 provider archive {
   version = "~> 1.3"
 }
+
+provider null {
+  version = "~> 2.1"
+}
--- testdata/v1_full/terraform/accounts/foo/fogg.tf
+++ testdata/v1_full/terraform/accounts/foo/fogg.tf
@@ -133,3 +133,7 @@ provider template {
 provider archive {
   version = "~> 1.3"
 }
+
+provider null {
+  version = "~> 2.1"
+}
