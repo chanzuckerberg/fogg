9d9b927b1f57710d84c6d6c03f7ba247398e6f78
Ryan King <rking@chanzuckerberg.com>
2021-01-14 13:40:00 -0800

    [fix] paths for new components paths command (#515)

--- cmd/components_paths.go
+++ cmd/components_paths.go
@@ -50,12 +50,12 @@ var componentsListCmd = &cobra.Command{
 		fmt.Println("terraform/global")
 
 		for _, a := range p.Accounts {
-			fmt.Printf("terraform/%s\n", a.AccountName)
+			fmt.Printf("terraform/accounts/%s\n", a.AccountName)
 		}
 
 		for _, e := range p.Envs {
 			for _, c := range e.Components {
-				fmt.Printf("terraform/%s/%s\n", e.Env, c.Component)
+				fmt.Printf("terraform/envs/%s/%s\n", e.Env, c.Component)
 			}
 		}
 		return nil
