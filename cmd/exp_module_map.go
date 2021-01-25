f819a0e8608f04fc3a309af8b62af0a0178310da
Ryan King <rking@chanzuckerberg.com>
2021-01-25 08:04:32 -0800

    [exp] module-map command (#506)
    Add an experimental command that, given a path will list all the local module dependencies. Will be useful for doing automation work with TFE.
    
    ### Test Plan
    * CI
    * manually tested on shared-infra
    
    ### References

---
+++ cmd/exp_module_map.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/chanzuckerberg/fogg/errs"
+	"github.com/chanzuckerberg/fogg/exp/modules"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	moduleMapCmd.Flags().String("path", "", "path to a working directory")
+	moduleMapCmd.Flags().StringP("config", "c", "fogg.yml", "Use this to override the fogg config file.")
+
+	expCmd.AddCommand(moduleMapCmd)
+}
+
+var moduleMapCmd = &cobra.Command{
+	Use:   "module-map",
+	Short: "",
+	Long:  ``,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Set up fs
+		pwd, err := os.Getwd()
+		if err != nil {
+			return errs.WrapUser(err, "can't get pwd")
+		}
+		fs := afero.NewBasePathFs(afero.NewOsFs(), pwd)
+
+		configFile, e := cmd.Flags().GetString("config")
+		if e != nil {
+			return errs.WrapInternal(e, "couldn't parse config flag")
+		}
+
+		openGitOrExit(fs)
+
+		path, _ := cmd.Flags().GetString("path")
+		return modules.Run(fs, configFile, path)
+	},
+}
---
+++ exp/modules/modules.go
@@ -0,0 +1,94 @@
+package modules
+
+import (
+	"fmt"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/chanzuckerberg/fogg/config"
+	fogg_hcl "github.com/chanzuckerberg/fogg/exp/hcl"
+	"github.com/chanzuckerberg/go-misc/sets"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+)
+
+func Run(fs afero.Fs, configFile, path string) error {
+	// figure out which component or account we are talking about
+	conf, err := config.FindAndReadConfig(fs, configFile)
+	if err != nil {
+		return err
+	}
+
+	component, err := conf.PathToComponentType(path)
+	if err != nil {
+		return err
+	}
+	logrus.Debugf("component kind %s", component.Kind)
+
+	sources, err := collectModuleSources(path)
+	if err != nil {
+		return err
+	}
+
+	logrus.Debugf("found sources %v", sources)
+
+	for _, s := range sources {
+		fmt.Println(s)
+	}
+
+	return nil
+}
+
+func collectModuleSources(path string) ([]string, error) {
+	references, err := extractLocalModules(path)
+	if err != nil {
+		return nil, err
+	}
+	refNames := references.List()
+	sort.Strings(refNames)
+	return refNames, nil
+}
+
+// extractLocalModules will recursively look for all local module references
+// we assume there are no cycles in this dependency graph
+func extractLocalModules(path string) (sets.StringSet, error) {
+	references := sets.StringSet{}
+
+	// for a given path, find all module references
+	err := fogg_hcl.ForeachBlock(path, func(block *hcl.Block) error {
+		if block.Type == "module" {
+			attrs, _ := block.Body.JustAttributes()
+			for _, a := range attrs {
+				if a.Name == "source" {
+					v, err := a.Expr.Value(nil)
+					if err != nil {
+						return err
+					}
+
+					if strings.HasPrefix(v.AsString(), "./") || strings.HasPrefix(v.AsString(), "../") {
+						references.Add(v.AsString())
+					}
+				}
+			}
+		}
+		return nil
+	})
+
+	// recursively find modules
+	for _, r := range references.List() {
+		t := filepath.Join(path, r)
+		refs, err := extractLocalModules(t)
+		if err != nil {
+			return references, err
+		}
+
+		// convert paths to be relative to the original path for use in tfe trigger prefixes
+		for _, x := range refs.List() {
+			p, _ := filepath.Rel(path, filepath.Join(t, x))
+			references.Add(p)
+		}
+	}
+	return references, err
+}
