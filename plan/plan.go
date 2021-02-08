d253a56c75f21ba571d6f1b4f8896a2bef2373ec
Ryan King <rking@chanzuckerberg.com>
2021-02-08 15:12:31 -0800

    add yaml tags

--- plan/plan.go
+++ plan/plan.go
@@ -230,8 +230,8 @@ type Module struct {
 type Account struct {
 	ComponentCommon `yaml:",inline"`
 
-	Account string
-	Global  *Component
+	Account string     `yaml:"account"`
+	Global  *Component `yaml:"global"`
 }
 
 // Component is a component
