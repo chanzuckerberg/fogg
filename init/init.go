dede7caeb71319d410e6f37a195416b4783a0e1b
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2022-10-14 13:44:47 -0400

    feat: upgrade default versions (#751)

--- init/init.go
+++ init/init.go
@@ -5,13 +5,13 @@ import (
 	"github.com/spf13/afero"
 )
 
-const AWSProviderVersion = "2.47.0"
+const AWSProviderVersion = "4.34.0"
 
 type FoggProject struct {
 	Project, Region, Bucket, Table, Profile, Owner *string
 }
 
-//Init reads user console input and generates a fogg.yml file
+// Init reads user console input and generates a fogg.yml file
 func Init(fs afero.Fs, foggProject *FoggProject) error {
 	config := config.InitConfig(
 		foggProject.Project,
