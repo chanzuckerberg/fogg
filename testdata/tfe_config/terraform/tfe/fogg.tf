fed7aa3dbe8296fd951d7397d229496bd7bf5de2
Jake Heath <jheath@chanzuckerberg.com>
2023-10-03 13:54:49 -0700

    fix: add a default value for potentially empty tags

--- testdata/tfe_config/terraform/tfe/fogg.tf
+++ testdata/tfe_config/terraform/tfe/fogg.tf
@@ -41,17 +41,17 @@ provider "aws" {
   # var.tags should be considered deprecated
   default_tags {
     tags = {
-      TFC_RUN_ID                               = var.TFC_RUN_ID
-      TFC_WORKSPACE_NAME                       = var.TFC_WORKSPACE_NAME
-      TFC_WORKSPACE_SLUG                       = var.TFC_WORKSPACE_SLUG
-      TFC_CONFIGURATION_VERSION_GIT_BRANCH     = var.TFC_CONFIGURATION_VERSION_GIT_BRANCH
-      TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
-      TFC_CONFIGURATION_VERSION_GIT_TAG        = var.TFC_CONFIGURATION_VERSION_GIT_TAG
-      TFC_PROJECT_NAME                         = var.TFC_PROJECT_NAME
-      project                                  = var.tags.project
-      env                                      = var.tags.env
-      service                                  = var.tags.service
-      owner                                    = var.tags.owner
+      TFC_RUN_ID                               = coalesce(var.TFC_RUN_ID, "unknown")
+      TFC_WORKSPACE_NAME                       = coalesce(var.TFC_WORKSPACE_NAME, "unknown")
+      TFC_WORKSPACE_SLUG                       = coalesce(var.TFC_WORKSPACE_SLUG, "unknown")
+      TFC_CONFIGURATION_VERSION_GIT_BRANCH     = coalesce(var.TFC_CONFIGURATION_VERSION_GIT_BRANCH, "unknown")
+      TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA = coalesce(var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA, "unknown")
+      TFC_CONFIGURATION_VERSION_GIT_TAG        = coalesce(var.TFC_CONFIGURATION_VERSION_GIT_TAG, "unknown")
+      TFC_PROJECT_NAME                         = coalesce(var.TFC_PROJECT_NAME, "unknown")
+      project                                  = coalesce(var.tags.project, "unknown")
+      env                                      = coalesce(var.tags.env, "unknown")
+      service                                  = coalesce(var.tags.service, "unknown")
+      owner                                    = coalesce(var.tags.owner, "unknown")
       managedBy                                = "terraform"
     }
   }
