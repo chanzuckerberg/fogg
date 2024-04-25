a3e8b568dd42abc9c2e230c6385308a832fa09fc
Jake Heath <jheath@chanzuckerberg.com>
2024-04-25 12:03:48 -0700

    remove custom provider

--- testdata/v2_full_yaml/terraform/envs/staging/k8s-comp/fogg.tf
+++ testdata/v2_full_yaml/terraform/envs/staging/k8s-comp/fogg.tf
@@ -68,6 +68,13 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
