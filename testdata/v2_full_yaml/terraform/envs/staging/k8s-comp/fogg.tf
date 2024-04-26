3812c7ae4ae694f96d8c026820edc7cda24af344
jakeyheath <jakeyheath@users.noreply.github.com>
2024-04-26 23:34:22 +0000

    commit from ci -- updated golden files

--- testdata/v2_full_yaml/terraform/envs/staging/k8s-comp/fogg.tf
+++ testdata/v2_full_yaml/terraform/envs/staging/k8s-comp/fogg.tf
@@ -65,7 +65,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 provider "helm" {
